bb2c696b3b1b15d928b64d439bb69d19b200e557
TobbetheStalker <tobbe.kling@hotmail.com>
2017-05-13 16:28:51 +0200

    UDP UPDATE

--- BachelorNetworking/BachelorNetworking/NetworkData.h
+++ BachelorNetworking/BachelorNetworking/NetworkData.h
@@ -5,7 +5,7 @@
 
 #define GIGABYTE = 1073741824;
 const int DATA_SIZE = 1073741824;
-const int UDP_BUFFER_SIZE = 5000000;
+const int UDP_BUFFER_SIZE = 100000;
 const int BUFFER_SIZE = 1073741824;
 
 #include <string>
