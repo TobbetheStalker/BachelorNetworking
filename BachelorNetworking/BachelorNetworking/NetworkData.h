ccf04e6e87c15804b65b86baea7283c4aa6e31c9
TobbetheStalker <tobbe.kling@hotmail.com>
2017-05-22 14:56:39 +0200

    UPDATE restored OS buffer back to 300MB

--- BachelorNetworking/BachelorNetworking/NetworkData.h
+++ BachelorNetworking/BachelorNetworking/NetworkData.h
@@ -7,7 +7,7 @@
 const int DATA_SIZE = 1073741824;
 const int UDP_BUFFER_SIZE = 100000;
 const int BUFFER_SIZE = 1024000 * 300;	//Mb * 300
-const int OS_BUFFERS = 1024000 * 600;
+const int OS_BUFFERS = 1024000 * 300;
 
 #include <string>
 #include <chrono>
